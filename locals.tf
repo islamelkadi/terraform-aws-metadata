9256402ccf827b0e1f759cb8af27c59bbfc70dcd
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-01-22 23:58:19 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -22,19 +22,46 @@ locals {
   region_name = data.aws_region.current.region
   region_code = local.region_codes[local.region_name]
   region_codes = {
+    # US Regions
     us-east-1      = "use1"
     us-east-2      = "use2"
     us-west-1      = "usw1"
     us-west-2      = "usw2"
+    
+    # EU Regions
     eu-west-1      = "euw1"
     eu-west-2      = "euw2"
     eu-west-3      = "euw3"
     eu-north-1     = "eun1"
-    ap-southeast-1 = "aps1"
-    ap-southeast-2 = "aps2"
-    ap-northeast-1 = "apn1"
-    ap-northeast-2 = "apn2"
-    ap-south-1     = "aps3"
+    eu-central-1   = "euc1"
+    eu-central-2   = "euc2"
+    eu-south-1     = "eus1"
+    eu-south-2     = "eus2"
+    
+    # Asia Pacific Regions
+    ap-southeast-1 = "apse1"
+    ap-southeast-2 = "apse2"
+    ap-southeast-3 = "apse3"
+    ap-southeast-4 = "apse4"
+    ap-northeast-1 = "apne1"
+    ap-northeast-2 = "apne2"
+    ap-northeast-3 = "apne3"
+    ap-south-1     = "aps1"
+    ap-south-2     = "aps2"
+    ap-east-1      = "ape1"
+    
+    # Canada Region
+    ca-central-1   = "cac1"
+    ca-west-1      = "caw1"
+    
+    # South America Region
     sa-east-1      = "sae1"
+    
+    # Middle East Regions
+    me-south-1     = "mes1"
+    me-central-1   = "mec1"
+    
+    # Africa Region
+    af-south-1     = "afs1"
   }
 }
