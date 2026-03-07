c03813c667136794829dd7ed4afb717af9b521fd
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-03-07 01:29:40 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,3 +1,14 @@
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_organizations_organization" "current" {}
+
+# Query available availability zones in the current region
+data "aws_availability_zones" "available" {
+  state = "available"
+
+  # Exclude local zones and wavelength zones
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
